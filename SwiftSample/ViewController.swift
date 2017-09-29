c46c695656ac0f85b4d2a68240790ae565bd5c88
vimalasohan <as_vimal@yahoo.co.in>
2017-09-29 09:19:31 -0400

    Basic UI Setup

--- SwiftSample/ViewController.swift
+++ SwiftSample/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import UIComponents
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    @IBOutlet var tableViewSample: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let bundle = Bundle(identifier: "com.UIComponents")
+        tableViewSample.register(UINib(nibName:"ReusableCell", bundle:bundle), forCellReuseIdentifier: "ReusableCell")
+        tableViewSample.estimatedRowHeight = 44.0
+        tableViewSample.rowHeight = UITableViewAutomaticDimension
+        //self.tableViewSample.register(ReusableCell.self, forCellReuseIdentifier: "ReusableCell")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = self.tableViewSample.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! ReusableCell
+        cell.cellTitleLabel.text = "VimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimal"
+        cell.cellImageView.image = UIImage(named: "Garden")
+        return cell
+    }
 
 }
 
