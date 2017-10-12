b7ee97049552e986bd476e5d5a5d8f04ce4d0d9d
vimalasohan <as_vimal@yahoo.co.in>
2017-10-12 17:49:52 -0400

    Integrating the service response to UITableView

--- SwiftSample/ViewController.swift
+++ SwiftSample/ViewController.swift
@@ -10,38 +10,42 @@ import UIKit
 import UIComponents
 import NetworkComponents
 
-class ViewController: UITableViewController {
-
+class ViewController:UIViewController {
     @IBOutlet var tableViewSample: UITableView!
     var tableRowCount = Int()
     var networkClass:NetworkConnection = NetworkConnection()
-    var responseString:Array = [Array<Any>()]
+    var responseString:Array<Any> = []
     override func viewDidLoad() {
         super.viewDidLoad()
         let bundle = Bundle(identifier: "com.UIComponents")
-        tableViewSample.register(UINib(nibName:"ReusableCell", bundle:bundle), forCellReuseIdentifier: "ReusableCell")
-        tableViewSample.estimatedRowHeight = 44.0
-        tableViewSample.rowHeight = UITableViewAutomaticDimension
-        networkClass.getJSONFromUrl(urlString: "http://services.groupkt.com/state/get/USA/all", completionHandler: {genres in
-            self.responseString.append(genres as! [Any])
-            self.tableRowCount = genres.count
+        self.tableViewSample.register(UINib(nibName:"ReusableCell", bundle:bundle), forCellReuseIdentifier: "ReusableCell")
+        self.tableViewSample.estimatedRowHeight = 44.0
+        self.tableViewSample.rowHeight = UITableViewAutomaticDimension
+        makeServiceCall()
+    }
+    func makeServiceCall()
+    {
+        networkClass.getJSONFromUrl(urlString:"http://services.groupkt.com/state/get/USA/all", completionHandler: {genres in
+            self.responseString = genres
+            self.tableRowCount = self.tableRowCount + genres.count
+            DispatchQueue.main.async {
+                self.tableViewSample.reloadData()
+            }
         })
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+}
+extension ViewController: UITableViewDataSource {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 30
+        return tableRowCount
     }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = self.tableViewSample.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! ReusableCell
-        cell.cellTitleLabel.text = "VimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimalVimal"
+        if self.responseString.count>0 {
+            let location : LocationItem = self.responseString[indexPath.row] as! LocationItem
+            cell.cellTitleLabel.text = location.name
+        }
         cell.cellImageView.image = UIImage(named: "Garden")
         return cell
     }
